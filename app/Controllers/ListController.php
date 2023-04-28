09688bdbef3a7845bb6a699cc1a6db83a9842e1c
xdloc <bretstiles0@gmail.com>
2023-04-28 22:16:36 +0700

    the initiation

--- app/Controllers/ListController.php
+++ app/Controllers/ListController.php
@@ -26,26 +26,26 @@ class ListController
     }
 
     /**
-     * @api /method?=list/addItem
+     * @api /method?=list/add
      *
      * @param  int  $listId
      * @param  string  $itemName
      * @return bool
      */
-    public function addItem(int $listId, string $itemName): bool
+    public function add(int $listId, string $itemName): bool
     {
         //todo check User access here
         return (new ItemBuilder())->createItem($listId, $itemName);
     }
 
     /**
-     * @api /method?=list/removeItem
+     * @api /method?=list/remove
      *
      * @param  int  $listId
      * @param  int  $itemId
      * @return bool
      */
-    public function removeItem(int $listId, int $itemId): bool
+    public function remove(int $listId, int $itemId): bool
     {
         // todo check User access here
         return (new Item())->delete($itemId);
@@ -53,14 +53,14 @@ class ListController
 
     /**
      * Edit item's name
-     * @api /method?=list/editItem
+     * @api /method?=list/edit
      *
      * @param  int  $listId
      * @param  int  $itemId
      * @param  string  $itemName
      * @return bool
      */
-    public function editItem(int $listId, int $itemId, string $itemName): bool
+    public function edit(int $listId, int $itemId, string $itemName): bool
     {
         // todo check User access here
         return (new Item())->update($listId, ['name' => $itemName]);
@@ -68,14 +68,14 @@ class ListController
 
     /**
      * Mark as completed or unmark it back
-     * @api /method?=list/markItem
+     * @api /method?=list/mark
      *
      * @param  int  $listId
      * @param  int  $itemId
      * @param  int  $status
      * @return bool
      */
-    public function markItem(int $listId, int $itemId, int $status): bool
+    public function mark(int $listId, int $itemId, int $status): bool
     {
         // todo check User access here
         return (new Item())->update($itemId, ['status' => $status]);
