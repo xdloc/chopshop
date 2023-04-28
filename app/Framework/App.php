b74ca7e0e6fb88b22a9d7c356819cea134d9ac02
xdloc <bretstiles0@gmail.com>
2023-04-28 18:55:08 +0700

    the initiation

--- app/Framework/App.php
+++ app/Framework/App.php
@@ -100,8 +100,8 @@ class App
     {
         header('Content-Type: application/json');
         header('Access-Control-Allow-Origin: https://chopshop-app.netlify.app/');
-        header('Access-Control-Allow-Headers: *');
-        header('Access-Control-Allow-Methods: *');
+        header('Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept');
+        header('Access-Control-Allow-Methods: GET,HEAD,PUT,PATCH,POST,DELETE');
         header('Access-Control-Allow-Credentials: true');
     }
 
